6d1b59ae29c1dd7e9e4b42daad8946dd2218cb88
Scott Wittenburg <scott.wittenburg@kitware.com>
2017-06-08 17:08:01 -0600

    Mark where collapsed group state should go

--- src/redux/ducks/ui.js
+++ src/redux/ducks/ui.js
@@ -19,6 +19,8 @@ export const initialState = {
     Representation: true,  // Closed
     View: true,            // Closed
     RenderViewSettingsCollapsed: false, // Open
+
+    collapsibleGroups: {},
   },
 };
 
